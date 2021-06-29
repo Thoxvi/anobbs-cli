d4813bc3ba46a6f7b4e4f0e54f228989f96cf10e
Thoxvi <A@Thoxvi.com>
2021-06-29 20:58:20 +0800

    update imcli

---
+++ imcli/core/__init__.py
@@ -0,0 +1,3 @@
+from .base_ui_object import *
+from .layout import *
+from .text import *
--- imcli/core/text.py
+++ imcli/core/text.py
@@ -3,21 +3,21 @@ from typing import AnyStr
 from imcli.core.base_ui_object import BaseUiObject
 
 
-def chunks_str(lst, n) -> AnyStr:
-    lines = []
-    for i in range(0, len(lst), n):
-        lines.append(lst[i:i + n])
-    return "\n".join(lines)
-
-
 class Text(BaseUiObject):
+    @classmethod
+    def chunks_str(cls, lst, n) -> AnyStr:
+        lines = []
+        for i in range(0, len(lst), n):
+            lines.append(lst[i:i + n])
+        return "\n".join(lines)
+
     def __init__(self, content: AnyStr, max_lenght: int = 32, **kwargs):
         super().__init__(**kwargs)
         self._max_lenght = max_lenght
         self.set_content(content)
 
     def set_content(self, content: AnyStr) -> bool:
-        self._content = chunks_str(content, self._max_lenght)
+        self._content = self.chunks_str(content, self._max_lenght)
         return True
 
 
